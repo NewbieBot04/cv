6392684fd7230548145b5f14db21fe7ef9bdcfcd
NitinYadav <75217255+NewbieBot04@users.noreply.github.com>
2021-01-17 15:53:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-from bs4 import BeautifulSoup
-import requests
-import smtplib
+import datetime as dt
+from notification_manager import NotificationManager
+from flight_data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import FlightData
+from ui import UserInterface
+from travelers_data_manager import TravelerData
 
-email = "lazyminds.0499@gmail.com"
-password = "8810nky@#0"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-                  " (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-           }
-url = "https://www.amazon.in/AmazonBasics-Full-Size-Ergonomic-Wireless-Scrolling/dp/B078HFRNSP/ref=sr_1_22_sspa?dchild" \
-      "=1&keywords=mouse&qid=1610741989&sr=8-22-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQ0ZFRlJER0M2MDY0JmVuY3J5c" \
-      "HRlZElkPUEwMzk1NzA1N0tNQkgwNFZMMUtZJmVuY3J5cHRlZEFkSWQ9QTA5Nzc2NzYxQjA5VjlRVjY1Q1hGJndpZGdldE5hbWU9c3BfYnRmJmFj" \
-      "dGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+ui = UserInterface()
+emails = ui.emails
+names = ui.names
+strip_email = emails[0].strip()
+i = 2
+for f_name in names:
+    split_name = f_name.split()
+    traveler_data = TravelerData(f_name=split_name[0].title(), l_name=split_name[1].title(), email=strip_email, index=i)
+    i += 1
 
-response = requests.get(url=url, headers=headers)
-site_data = response.text
+nxtday_date = dt.datetime.today() + dt.timedelta(days=1)
+nxtday = nxtday_date.strftime("%d/%m/%Y")
+to_date = "1/3/2021"
 
-soup = BeautifulSoup(site_data, "lxml")
-price_tag = soup.find(name="span", id="priceblock_ourprice")
-price = price_tag.string
-strip_price = price.split()
-actual_price = strip_price[1].replace(",", "")
-name_tag = soup.find(name="span", id="productTitle")
-product_name = name_tag.string
+data_manager = DataManager()
+travel_data = data_manager.getting_data()
+new_lst = []
+for i in range(len(travel_data["sheet1"])):
+    print(i)
+    destination_iata_code = travel_data["sheet1"][i]["iata"]
+    min_fare = travel_data["sheet1"][i]["lowestPrice"]
+    flight_search = FlightSearch(fly_from="DEL", fly_to=destination_iata_code, from_date=nxtday,
+                                 to_date=to_date, min_price=min_fare)
+    flight_search_data = flight_search.searching()
+    flight_data = FlightData(flight_search_data)
+    new_lst.append(flight_data.processing())
+print(new_lst)
 
+msg_from = ""
+msg_to = ""
+msg_fair = 0
+msg_seats = 0
 
-if float(actual_price) < 1500:
-    connection = smtplib.SMTP("smtp.gmail.com", port=587)
-    connection.starttls()
-    connection.login(user=email, password=password)
-    connection.sendmail(from_addr=email, to_addrs="lazyminds.0499@yahoo.com", msg=f"Subject:Amazon price Alert"
-                                                                                  f"\n\n{product_name}"
-                                                                                  f" available at price "
-                                                                                  f"{actual_price} \n {url}"
-                        )
-    connection.close()
+for i in new_lst:
+    if i:
+        msg_to = i[1]
+        msg_fair = i[2]
+        msg_seats = i[3]
+    msg = NotificationManager(price=msg_fair, destination=msg_to,
+                              from_=nxtday, to_=to_date,
+                              available=msg_seats, email=strip_email)
+    msg.sending_mail()
